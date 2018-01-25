3508d833062b3ea2003fc4e505956d02a095b795
Mikhail Maslyuk <leakymee@bu.edu>
2018-01-24 21:47:55 -0500

    Rough draft D3 data formatting

--- ecommerce-api-server/vantage-api/controllers/salesReports.js
+++ ecommerce-api-server/vantage-api/controllers/salesReports.js
@@ -18,18 +18,45 @@ module.exports.aggregateSalesData = async function(req, res, next) {
 									.map(ticket => ticket.items)
 									.reduce((acc, cur) => acc.concat(cur))
 		const allMenuItemsSoldByItem = _.groupBy(allMenuItemsSold, 'itemName')
+		const allMenuItemsSoldByCategory = _.groupBy(allMenuItemsSold, 'category')
 		const allMenuItemsSoldKeys = Object.keys(allMenuItemsSoldByItem);
 		const grossSalesByMenuItemObject = {}
+		const grossSalesByServer = {}
+		const grossSalesByMenuItemCategory = {}
+		/*
 		const grossSalesByMenuItemArray = allMenuItemsSoldKeys.map(key => allMenuItemsSoldByItem[key]
 																		.map(arrayOfSoldMenuItemsInCategory => new BigNumber(arrayOfSoldMenuItemsInCategory.itemPrice))
 																		.reduce((acc, cur) => acc.plus(cur))); // creates an array instead of an object
-		const menuItemsGross = Object.keys(allMenuItemsSoldByItem).forEach(key => grossSalesByMenuItemObject[key] = allMenuItemsSoldByItem[key]
+		*/
+
+		// Can remove the consts 
+		// Can abstract these out into a single function
+		const menuItemsGross = Object.keys(allMenuItemsSoldByItem)
+										.map(key => {
+										grossSalesByMenuItemObject[key] = allMenuItemsSoldByItem[key]
+										.map(arrayOfSoldMenuItemsInCategory => new BigNumber(arrayOfSoldMenuItemsInCategory.itemPrice))
+										.reduce((acc, cur) => acc.plus(cur)).toNumber()
+										return ({[key]:grossSalesByMenuItemObject[key]})
+								}) 
+		
+
+		
+		const serverTallyGross = Object.keys(allTicketsByServer).forEach(key => grossSalesByServer[key] = allTicketsByServer[key]
+																		.map(arrayOfSoldMenuItemsInCategory => new BigNumber(arrayOfSoldMenuItemsInCategory.totalReal))
+																		.reduce((acc, cur) => acc.plus(cur)).toNumber())
+		const menuItemCategory = Object.keys(allMenuItemsSoldByCategory).map(key => grossSalesByMenuItemCategory[key] = allMenuItemsSoldByCategory[key]
 																		.map(arrayOfSoldMenuItemsInCategory => new BigNumber(arrayOfSoldMenuItemsInCategory.itemPrice))
-																		.reduce((acc, cur) => acc.plus(cur)).toNumber()) // mutates allMenuItemsSoldByItem
+																		.reduce((acc, cur) => acc.plus(cur)).toNumber())
+		const grossSalesByServerArray = []
+		const grossSalesByMenuItemCategoryArray = []
+		const grossSalesByMenuItemArray = []
+		Object.keys(allMenuItemsSoldByItem).forEach(key => grossSalesByMenuItemArray.push({	[key]:allMenuItemsSoldByItem[key] }))
+		Object.keys(allMenuItemsSoldByCategory).forEach(key => grossSalesByMenuItemCategoryArray.push({	[key]:allMenuItemsSoldByCategory[key] }))
+		Object.keys(allTicketsByServer).forEach(key => grossSalesByServerArray.push({	[key]:allTicketsByServer[key] }))
 
 
-
-		const allMenuItemsSoldByCategory = _.groupBy(allMenuItemsSold, 'category')
+		
+		
 
 		const grossSales = allTicketsBySession
 						   	.map(ticket => new BigNumber(ticket.totalReal))
@@ -44,8 +71,14 @@ module.exports.aggregateSalesData = async function(req, res, next) {
 			all_menu_items_sold_by_item: allMenuItemsSoldByItem,
 			all_menu_items_sold_by_category: allMenuItemsSoldByCategory,
 			gross_sales: grossSales,
-			gross_sale_by_menu_item_array: grossSalesByMenuItemArray,
+			gross_sale_by_menu_item_array_d3: grossSalesByMenuItemArray,
+			gross_sales_by_server_array_d3: grossSalesByServerArray,
+			gross_sales_by_menu_item_category_array_d3: grossSalesByMenuItemCategoryArray,
 			gross_sales_by_menu_item: grossSalesByMenuItemObject,
+			gross_sales_by_server: grossSalesByServer,
+			gross_sales_by_menu_item_category: grossSalesByMenuItemCategory,
+			test: menuItemsGross,
+
 		}
 		res.json(data)
 		/*
