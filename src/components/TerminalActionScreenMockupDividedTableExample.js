bb34779a3424675bced8f8164c1cd6d070d78495
Mikhail Maslyuk <leakymee@bu.edu>
2018-02-14 04:42:24 -0500

    Added some design to table views

---
+++ src/components/TerminalActionScreenMockupDividedTableExample.js
@@ -0,0 +1,762 @@
+import React, { Component } from 'react'
+import { Table, Button, Icon } from 'semantic-ui-react'
+class TerminalActionScreenMockup extends Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return(
+		<div className="action-screen-wrapper" >
+			<div className="left-40" >
+				<div className="header-container" >
+
+				<Table celled inverted selectable>
+					
+					<Table.Header>
+						<Table.Row>
+							<Table.HeaderCell> Remove Item </Table.HeaderCell>
+							<Table.HeaderCell> Add-On </Table.HeaderCell>
+							<Table.HeaderCell> Item Name </Table.HeaderCell>
+							<Table.HeaderCell> Price </Table.HeaderCell>
+						</Table.Row>
+					</Table.Header>
+
+				</Table>
+
+				</div>
+				<div className="table-body-container" >
+				
+				<Table celled inverted selectable>
+					
+					<Table.Body>
+						
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+
+						<Table.Row>
+							
+							<Table.Cell>
+								<Button inverted> Remove Item </Button>
+							</Table.Cell>
+							<Table.Cell>
+								<Button inverted> <Icon name='plus' /> </Button>
+							</Table.Cell>
+							<Table.Cell>
+								Turkey Legs MyDude
+							</Table.Cell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+
+						</Table.Row>
+
+					</Table.Body>
+
+					</Table>
+
+					</div>
+					
+					<div className='footer-container' >
+					<Table>
+					<Table.Footer>
+						
+						<Table.Row>
+							<Table.HeaderCell colSpan='3'>
+								Subtotal
+							</Table.HeaderCell>
+							<Table.Cell>
+								$ 19.43
+							</Table.Cell>
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.HeaderCell colSpan='3'>
+								Tax
+							</Table.HeaderCell>
+							<Table.Cell> 
+								$ 0.42
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+							
+							<Table.HeaderCell colSpan='3'>
+								Total
+							</Table.HeaderCell>
+							<Table.Cell>
+								$ 19.85
+							</Table.Cell>
+
+						</Table.Row>
+
+						<Table.Row>
+
+							<Table.HeaderCell colSpan='3'>
+								Discount
+							</Table.HeaderCell>
+							<Table.Cell>
+								$ 0.00
+							</Table.Cell>
+						</Table.Row>
+
+					</Table.Footer>
+
+				</Table>
+				</div>
+			</div>
+			<div className="right-60" >
+
+			</div>
+		</div>
+		)
+	}
+}
+
+export default TerminalActionScreenMockup;
