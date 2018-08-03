4580c9cdaa5b82b16a2067657105b92b2d062c64
unknown <leakymee@bu.edu>
2018-08-03 18:41:44 -0400

    Revamp Admin Terminal UI

--- src/components/EssosMarket.js
+++ src/components/EssosMarket.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import './styles/EssosMarket.css';
 
 import { retrieveAllItemsForSale, retrieveShoppingCart } from '../actions/marketplace'
--- src/components/terminal/admin/AdminTerminal.js
+++ src/components/terminal/admin/AdminTerminal.js
@@ -2,25 +2,46 @@ import React, { Component } from 'react'
 import '../../styles/AdminTerminal.css'
 
 class AdminTerminal extends Component {
+	state = {
+		visibleCategory: 'employees'
+	}
+
 	render() {
 		return(
 			<div className='admin-page-wrapper'>
-				<div className='sidebar-column'>
-					<div className='menu-box'>
-						<div className='admin-button'>
-							Access Point-Of-Sale
+				<div className='app-header-admin'>
+				</div>
+				<div className='main-body-wrapper' >
+					<div className='sidebar-column'>
+						<div className='menu-box'>
+							<div className='admin-button'>
+								<img className='admin-menu-icon' src='./assets/icons/emp_mng.svg' />
+								<span> Employees </span>
+							</div>
+							<div className='admin-button'>
+								<img className='admin-menu-icon' src='./assets/icons/analytics.svg' />
+								<span> Sales Reports </span>
+							</div>
+							<div className='admin-button'>
+								<img className='admin-menu-icon' src='./assets/icons/calendar.svg' />
+								<span> Timesheets </span>
+							</div>					
 						</div>
-						<div className='admin-button'>
-							Manage Employees
+						<div className="menu-box__bottom">
+							<div className='admin-button'>
+								<img className='admin-menu-icon' src='./assets/icons/cash-register.svg' />
+								<span> Access Terminal </span>
+							</div>
+							<div className='admin-button'>
+								<img className='admin-menu-icon' src='./assets/icons/logout.svg' />
+								<span> Log Out </span>
+							</div>		
 						</div>
-						<div className='admin-button'>
-							View Statistics
-						</div>					
 					</div>
-				</div>
-				<div className='action-column'>
-				</div>
-				<div className='feed-column'>
+					<div className='action-column'>
+					</div>
+					<div className='feed-column'>
+					</div>
 				</div>
 			</div>
 		)
