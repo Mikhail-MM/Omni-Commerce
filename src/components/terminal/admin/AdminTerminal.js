6c9dac52ce90575a1f6f7d969e2e9bcdd010c921
unknown <leakymee@bu.edu>
2018-08-03 23:40:21 -0400

    Made simple prototype for Employee Management Table

--- src/components/terminal/admin/AdminTerminal.js
+++ src/components/terminal/admin/AdminTerminal.js
@@ -3,7 +3,14 @@ import '../../styles/AdminTerminal.css'
 
 class AdminTerminal extends Component {
 	state = {
-		visibleCategory: 'employees'
+		visibleCategory: 'employees',
+		workerSearch: ''
+	}
+
+	handleChange = (key, value) => {
+		this.setState({
+			[key]: value
+		})
 	}
 
 	render() {
@@ -39,6 +46,173 @@ class AdminTerminal extends Component {
 						</div>
 					</div>
 					<div className='action-column'>
+						<div className='employee-actions-bar'>
+							<div className='add-employee-button'>
+								<img className='admin-menu-icon' src='./assets/icons/add-user.svg' />
+								Add Employee
+							</div>
+							<div className='admin-employee-search-bar-container'>
+								<img className='admin-menu-icon' src='./assets/icons/worker.svg' />
+								<input style={{flexGrow: 1}}type='text' value={this.state.workerSearch} onChange={(e) => this.handleChange('workerSearch', e.target.value)}/>
+							</div>
+						</div>
+						<div className='employee-table-container'>
+							<table className='employee-table'>
+								<thead className='employee-table-head'>
+									<tr>
+										<td> Photo </td>
+										<td> Name </td>
+										<td> Phone </td>
+										<td> Email </td>
+										<td> 86 </td>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/44.jpg'/> </td>
+										<td> Emily Kim</td>
+										<td> (904)-751-4123 </td>
+										<td> eK41@gmail.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/32.jpg'/> </td>
+										<td> Adrian Chavez </td>
+										<td> (904)-751-4123 </td>
+										<td> bigmUn3y@exxon.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/44.jpg'/> </td>
+										<td> Emily Kim</td>
+										<td> (904)-751-4123 </td>
+										<td> eK41@gmail.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/32.jpg'/> </td>
+										<td> Adrian Chavez </td>
+										<td> (904)-751-4123 </td>
+										<td> bigmUn3y@exxon.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/44.jpg'/> </td>
+										<td> Emily Kim</td>
+										<td> (904)-751-4123 </td>
+										<td> eK41@gmail.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/32.jpg'/> </td>
+										<td> Adrian Chavez </td>
+										<td> (904)-751-4123 </td>
+										<td> bigmUn3y@exxon.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/44.jpg'/> </td>
+										<td> Emily Kim</td>
+										<td> (904)-751-4123 </td>
+										<td> eK41@gmail.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/32.jpg'/> </td>
+										<td> Adrian Chavez </td>
+										<td> (904)-751-4123 </td>
+										<td> bigmUn3y@exxon.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/44.jpg'/> </td>
+										<td> Emily Kim</td>
+										<td> (904)-751-4123 </td>
+										<td> eK41@gmail.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/32.jpg'/> </td>
+										<td> Adrian Chavez </td>
+										<td> (904)-751-4123 </td>
+										<td> bigmUn3y@exxon.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/44.jpg'/> </td>
+										<td> Emily Kim</td>
+										<td> (904)-751-4123 </td>
+										<td> eK41@gmail.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/32.jpg'/> </td>
+										<td> Adrian Chavez </td>
+										<td> (904)-751-4123 </td>
+										<td> bigmUn3y@exxon.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/44.jpg'/> </td>
+										<td> Emily Kim</td>
+										<td> (904)-751-4123 </td>
+										<td> eK41@gmail.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/32.jpg'/> </td>
+										<td> Adrian Chavez </td>
+										<td> (904)-751-4123 </td>
+										<td> bigmUn3y@exxon.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/44.jpg'/> </td>
+										<td> Emily Kim</td>
+										<td> (904)-751-4123 </td>
+										<td> eK41@gmail.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/32.jpg'/> </td>
+										<td> Adrian Chavez </td>
+										<td> (904)-751-4123 </td>
+										<td> bigmUn3y@exxon.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/44.jpg'/> </td>
+										<td> Emily Kim</td>
+										<td> (904)-751-4123 </td>
+										<td> eK41@gmail.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/32.jpg'/> </td>
+										<td> Adrian Chavez </td>
+										<td> (904)-751-4123 </td>
+										<td> bigmUn3y@exxon.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/44.jpg'/> </td>
+										<td> Emily Kim</td>
+										<td> (904)-751-4123 </td>
+										<td> eK41@gmail.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+									<tr>
+										<td> <img className='employee-avatar-image' src='./assets/avatars/32.jpg'/> </td>
+										<td> Adrian Chavez </td>
+										<td> (904)-751-4123 </td>
+										<td> bigmUn3y@exxon.com </td>
+										<td> <div className='fireButton'> Revoke Access </div> </td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+
+
 					</div>
 					<div className='feed-column'>
 					</div>
