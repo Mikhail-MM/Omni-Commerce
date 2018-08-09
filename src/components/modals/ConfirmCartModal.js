d7fca93c97b287ba605749e9436c313a25592ec6
unknown <leakymee@bu.edu>
2018-08-09 01:10:36 -0400

    Fixed bug related to  AddItemToCartModal's 'Add To Cart' onClick being an IIFE, causing cart overflow whenever the modal was mounted

--- src/components/modals/ConfirmCartModal.js
+++ src/components/modals/ConfirmCartModal.js
@@ -37,14 +37,17 @@ class ConfirmCartModal extends Component {
 		console.log("Looking if item is already in cart, before adding item to shopping cart, existingCountInCart defaulting to 0")
 		let existingCountInCart = 0
 		// WARNING: If we don't have a cached shopping cart, this check will fail and we will add too much to the cart. Could return a dispatch to fetch or just let failsafes take care of it
-		if (shoppingCart.itemsBought.find(element => element.itemRef_id == itemID)) {
-			console.log("Item that user is adding to cart already exists as entity in their shopping cart by itemRef_id")
+		if (shoppingCart.itemsBought.find(element => element.itemRef_id == itemID)) { 
+			console.log("Why is this happening with no element found...")
+			console.log(shoppingCart.itemsBought.find(element => element.itemRef_id == itemID))
+			
 			const itemIndex = shoppingCart.itemsBought.findIndex(element => element.itemRef_id == itemID)
-			console.log("Sending Updating existingCountInCart To Dispatch: ")
+			console.log("Item that user is adding to cart already exists as entity in their shopping cart by itemRef_id at index: ", itemIndex)
+			console.log("")
 			existingCountInCart = shoppingCart.itemsBought[itemIndex].numberRequested
 			console.log(existingCountInCart);
 		}
-		this.props.pushItemIntoShoppingCart(token, itemID, this.state.requestedAmount, existingCountInCart)
+		return this.props.pushItemIntoShoppingCart(token, itemID, this.state.requestedAmount, existingCountInCart)
 	}
 
 	render() {
@@ -60,7 +63,7 @@ class ConfirmCartModal extends Component {
 					<h3> Confirm your Order! </h3>
 					<h4> Confirm your purchase of {item.itemName} </h4>
 					<input type='number' value={this.state.requestedAmount} onChange={e => this.handleChange('requestedAmount', e.target.value)} />
-					<button onClick={this.addItemToCart(item._id)}>Add To Cart</button>
+					<button onClick={() => this.addItemToCart(item._id)}>Add To Cart</button>
 
 					<button onClick={() => this.props.hideModal()}> Cancel </button>
 
