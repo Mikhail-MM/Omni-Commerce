ceff3a39a7219593a5aac48bba197357c5e288cc
Mikhail Maslyuk <leakymee@bu.edu>
2018-09-10 17:25:00 -0400

    Wireframe splash page content layout and added animations + scroll functions for finding centered refs

--- src/components/Marketing.js
+++ src/components/Marketing.js
@@ -12,7 +12,8 @@ const mapDispatchToProps = dispatch => ({
 
 class  Marketing extends Component {
 	state = { 
-		topOfPage: true, 
+		topOfPage: true,
+		activeFeature: null, 
 		scrollDir: null,
 	}
 	componentDidMount() {
@@ -25,8 +26,20 @@ class  Marketing extends Component {
 
 	uniqueCollisionFreePrevScrollTop = null
 
+	determineActiveFeature = (viewportCenter, objectOfVerticallyStackedRows) => {
+		Object.keys(objectOfVerticallyStackedRows).forEach(featureKey => {
+			const domRECT = objectOfVerticallyStackedRows[featureKey]
+			if ((domRECT.top <= viewportCenter) && (domRECT.bottom >= viewportCenter)) return this.setState({
+				activeFeature: featureKey
+			})
+		})
+	}
+
 	handleScroll = (event) => {
 		let scrollTop = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop)
+		let viewportCenter = (window.innerHeight / 2)
+		
+		this.determineActiveFeature(viewportCenter, {ft1 : this.ft1.getBoundingClientRect(), ft2: this.ft2.getBoundingClientRect(), ft3: this.ft3.getBoundingClientRect(), ft4: this.ft4.getBoundingClientRect()})
 
 		if (this.uniqueCollisionFreePrevScrollTop && scrollTop > this.uniqueCollisionFreePrevScrollTop && this.state.scrollDir !== 'Scrolling Down') { 
 			this.setState({
@@ -106,10 +119,10 @@ class  Marketing extends Component {
 						<h2> Payments Made Easy! </h2>
 						<p> Omni is an E-Commerce platform aimed at empowering small businesses and individual entrepreneurs by facilitating the ability to easily accept cash and credit card payments. </p>  
 					</div>
-					<div style={{width: '100vw', height: 5000}} ref={el => this.stickyElContainer = el}>
+					<div style={{width: '100%', height: 'auto', backgroundColor: '#AA3939'}} ref={el => this.stickyElContainer = el}>
 						<div className={`icon-revealer-container${(this.state.manageSticky) ? ' shrink-sticky' : ''}`} ref={el => this.stickyEl = el}>
 							<div className='icon-revealer-row'>
-								<div ref={el => this.img1 = el} className={`informatic-blurb abs-ico-co1${(this.state.manageSticky) ? ' co1-move' : ''}`}>
+								<div ref={el => this.img1 = el} style={(this.state.activeFeature === 'ft1') ? {animation: 'hoverme 0.8s infinite ease-out'} : {}} className={`informatic-blurb abs-ico-co1${(this.state.manageSticky) ? ' co1-move' : ''}`}>
 									<div className={`informatic-blurb__icon${(this.state.manageSticky) ? ' shrink-icon' : ''}`}>
 										<img src='/assets/omni-splash/icons/payment-method.svg' />
 									</div>
@@ -117,7 +130,7 @@ class  Marketing extends Component {
 									<div className='informatic-blurb__text'>
 									</div>
 								</div>
-								<div ref={el => this.img2 = el} className={`informatic-blurb abs-ico-co2${(this.state.manageSticky) ? ' co2-move' : ''}`}>
+								<div ref={el => this.img2 = el} style={(this.state.activeFeature === 'ft2') ? {animation: 'hoverme 0.8s infinite ease-out'} : {}} className={`informatic-blurb abs-ico-co2${(this.state.manageSticky) ? ' co2-move' : ''}`}>
 									<div className={`informatic-blurb__icon${(this.state.manageSticky) ? ' shrink-icon' : ''}`}>
 										<img src='/assets/omni-splash/icons/online-shop.svg' />
 									</div>
@@ -125,7 +138,7 @@ class  Marketing extends Component {
 									<div className='informatic-blurb__text'>
 									</div>
 								</div>
-								<div ref={el => this.img3 = el} className={`informatic-blurb abs-ico-co3${(this.state.manageSticky) ? ' co3-move' : ''}`}>
+								<div ref={el => this.img3 = el} style={(this.state.activeFeature === 'ft3') ? {animation: 'hoverme 0.8s infinite ease-out'} : {}} className={`informatic-blurb abs-ico-co3${(this.state.manageSticky) ? ' co3-move' : ''}`}>
 									<div className={`informatic-blurb__icon${(this.state.manageSticky) ? ' shrink-icon' : ''}`}>
 										<img src='/assets/omni-splash/icons/stats.svg' />						
 									</div>
@@ -133,7 +146,7 @@ class  Marketing extends Component {
 									<div className='informatic-blurb__text'>
 									</div>
 								</div>
-								<div ref={el => this.img4 = el} className={`informatic-blurb abs-ico-co4${(this.state.manageSticky) ? ' co4-move' : ''}`}>
+								<div ref={el => this.img4 = el} style={(this.state.activeFeature === 'ft4') ? {animation: 'hoverme 0.8s infinite ease-out'} : {}} className={`informatic-blurb abs-ico-co4${(this.state.manageSticky) ? ' co4-move' : ''}`}>
 									<div className={`informatic-blurb__icon${(this.state.manageSticky) ? ' shrink-icon' : ''}`}>
 										<img src='/assets/omni-splash/icons/smartphone.svg' />
 									</div>
@@ -143,7 +156,52 @@ class  Marketing extends Component {
 								</div>
 							</div>
 						</div>
-						<div style={{width: '100vw', height: 5000}} />
+						<div className='feature-set-container'>
+							<div ref={el => this.ft1 = el } className={`feature-padded-row${(this.state.activeFeature === 'ft1') ? ' activate-feature' : ''}`} >
+								<div className='feature-blurb feature-blurb__text'>
+									<p> Wow, really great cool feature here. Absolutely incredible. U will luv dis!! </p>
+									<p> Works as intended let us bring the overlord. Absolute infallible yesu. Keep it coming keep it coming that's the great thing about it! WHeeee!!! </p>
+									<p> Wow, really great cool feature here. Absolutely incredible. U will luv dis!! </p>
+									<p> Works as intended let us bring the overlord. Absolute infallible yesu. Keep it coming keep it coming that's the great thing about it! WHeeee!!! </p>
+								</div>
+								<div className='feature-blurb__image'>
+									<img src={'/assets/externalReference/profilepagedesign.png'}/>
+								</div>
+							</div>
+							<div ref={el => this.ft2 = el } className={`feature-padded-row${(this.state.activeFeature === 'ft2') ? ' activate-feature' : ''}`}>
+								<div className='feature-blurb'>
+									<img src={'/assets/externalReference/profilepagedesign.png'}/>
+								</div>
+								<div className='feature-blurb feature-blurb__text'>
+									<p> Wow, really great cool feature here. Absolutely incredible. U will luv dis!! </p>
+									<p> Works as intended let us bring the overlord. Absolute infallible yesu. Keep it coming keep it coming that's the great thing about it! WHeeee!!! </p>
+									<p> Wow, really great cool feature here. Absolutely incredible. U will luv dis!! </p>
+									<p> Works as intended let us bring the overlord. Absolute infallible yesu. Keep it coming keep it coming that's the great thing about it! WHeeee!!! </p>
+								</div>
+							</div>
+							<div ref={el => this.ft3 = el } className={`feature-padded-row${(this.state.activeFeature === 'ft3') ? ' activate-feature' : ''}`}>
+								<div className='feature-blurb feature-blurb__text'>
+									<p> Wow, really great cool feature here. Absolutely incredible. U will luv dis!! </p>
+									<p> Works as intended let us bring the overlord. Absolute infallible yesu. Keep it coming keep it coming that's the great thing about it! WHeeee!!! </p>
+									<p> Wow, really great cool feature here. Absolutely incredible. U will luv dis!! </p>
+									<p> Works as intended let us bring the overlord. Absolute infallible yesu. Keep it coming keep it coming that's the great thing about it! WHeeee!!! </p>
+								</div>
+								<div className='feature-blurb'>
+									<img src={'/assets/externalReference/profilepagedesign.png'}/>
+								</div>
+							</div>
+							<div ref={el => this.ft4 = el } className={`feature-padded-row${(this.state.activeFeature === 'ft4') ? ' activate-feature' : ''}`}>
+								<div className='feature-blurb'>
+									<img src={'/assets/externalReference/profilepagedesign.png'}/>
+								</div>
+								<div className='feature-blurb feature-blurb__text'>
+									<p> Wow, really great cool feature here. Absolutely incredible. U will luv dis!! </p>
+									<p> Works as intended let us bring the overlord. Absolute infallible yesu. Keep it coming keep it coming that's the great thing about it! WHeeee!!! </p>
+									<p> Wow, really great cool feature here. Absolutely incredible. U will luv dis!! </p>
+									<p> Works as intended let us bring the overlord. Absolute infallible yesu. Keep it coming keep it coming that's the great thing about it! WHeeee!!! </p>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			)
