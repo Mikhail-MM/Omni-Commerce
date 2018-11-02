056cdf61934c1dc2eb009e572ea0290cbafdb8bd
Mikhail Maslyuk <leakymee@bu.edu>
2018-11-01 20:35:42 -0700

    Attempt to fix header stuttering with else-if but still occurs

--- src/components/Marketing.js
+++ src/components/Marketing.js
@@ -76,7 +76,7 @@ class  Marketing extends Component {
 			})
 		}
 
-		if (this.stickyElContainer.getBoundingClientRect().top < (stickyElementRef.getBoundingClientRect().top - 50) && !this.state.manageSticky) { 
+		if (this.stickyElContainer.getBoundingClientRect().top < (stickyElementRef.getBoundingClientRect().top - 65) && !this.state.manageSticky) { 
 			this.setState({
 				manageSticky: true
 			})
